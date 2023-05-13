87e88c0b88fd2846335cbb605c80e71c637069d2
Evgeny Golovin <yokopoomy20@gmail.com>
2023-05-13 22:51:32 +0300

    Fix

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -5,11 +5,11 @@ console.log(tabContents);
 tabs.forEach((el, index) => {
 	el.addEventListener('click', function(e) {
 		//tabs.findIndex( item => item.classname.includes('tab_active'))
-		const index1 = tabs.findIndex( item => item.className == 'tab tab_active');
+		const index1 = tabs.findIndex( item => item.className.includes('tab_active'));
 		tabs[index1].classList.remove('tab_active');
 		el.classList.toggle('tab_active');
 
-		const index2 = tabContents.findIndex( item => item.className == 'tab__content tab__content_active');
+		const index2 = tabContents.findIndex( item => item.className.includes('tab__content_active'));
 		tabContents[index2].classList.remove('tab__content_active');
 		tabContents[index].classList.toggle('tab__content_active');
 		console.log(tabContents[index].className);
