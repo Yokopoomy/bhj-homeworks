2f25aa071bf938fb1a268611bcb50a3512a9aef6
Evgeny Golovin <yokopoomy20@gmail.com>
2023-05-14 06:37:44 +0300

    Fix2

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,18 +1,16 @@
 const tabs = [].slice.call(document.querySelectorAll('.tab'));
 const tabContents = [].slice.call(document.querySelectorAll('.tab__content'));
-console.log(tabContents);
+
 
 tabs.forEach((el, index) => {
 	el.addEventListener('click', function(e) {
-		//tabs.findIndex( item => item.classname.includes('tab_active'))
-		const index1 = tabs.findIndex( item => item.className.includes('tab_active'));
-		tabs[index1].classList.remove('tab_active');
+
+		const tabIndex = tabs.findIndex( item => item.className.includes('tab_active'));
+		tabs[tabIndex].classList.remove('tab_active');
 		el.classList.toggle('tab_active');
 
-		const index2 = tabContents.findIndex( item => item.className.includes('tab__content_active'));
-		tabContents[index2].classList.remove('tab__content_active');
+		tabContents[tabIndex].classList.remove('tab__content_active');
 		tabContents[index].classList.toggle('tab__content_active');
-		console.log(tabContents[index].className);
 
 	})
 }, false)
